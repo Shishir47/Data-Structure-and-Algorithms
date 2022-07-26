4db9f40a4e91cc8583a64ac36adef36e6ddf1337
Raufun Nasim Khan Chowdhury <44887399+Shishir47@users.noreply.github.com>
2022-07-27 00:13:28 +0600

    Add files via upload

---
+++ InfixToPostFix/src/Main.java
@@ -0,0 +1,12 @@
+public class Main extends InfixToPostFix{
+
+		public Main(int size) {
+		super(size);
+	}
+
+		public static void main(String[] args) 
+	    {
+	        System.out.println(InfixToPostFix("6+(2*3)"));
+	    }
+	}
+
---
+++ InfixToPostFix/src/Stack.java
@@ -0,0 +1,60 @@
+public class Stack<E>{
+	private int top=-1;
+	private int size;
+	E[] data;
+	public Stack(int size) {
+		this.size=size;
+		data=(E[]) new Object [size];
+	}
+	public int size()
+	{
+		return (top+1);
+	}
+	public boolean isFull() {
+		if(top==(size-1)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public boolean isEmpty() {
+		if(top==-1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public void push(E item) {
+		if(isFull()) {
+			System.out.println("Stack is full!");
+		}
+		else {
+			top++;
+			data[top] = item;
+		}
+	}
+	public E pop() {
+		if(!isEmpty()) {
+			E item= data[top];
+			data[top]=null;
+			top--;
+			return item;
+		}
+		else {
+			System.out.println("Stack is empty!");
+			return null;
+		}
+	}
+	public E peek() {
+		if(!isEmpty()) {
+			return data[top];
+		}
+		else {
+			System.out.println("Stack is empty!");
+			return null;
+		}
+	}
+}
+
